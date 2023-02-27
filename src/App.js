88ad4e673f1b2d4cfde8ec5947444d8c81dbe1a5
SALMAN ANSARI <salmanansari02120@gmail.com>
2023-02-27 13:19:20 +0530

    added pages template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Frequently from "./components/Frequently";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
+import Inventory from "./components/Inventory";
+import Proposel from "./components/Proposel";
+import Services from "./components/Services";
+import Track from "./components/Track";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Frequently/>
+      <Services/>
+      <Track/>
+      <Inventory/>
+      <HowItWorks/>
+      <Proposel/>
+      <Footer/>
     </div>
   );
 }
